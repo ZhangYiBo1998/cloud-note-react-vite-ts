8a8ef9e4f6d1b60b4e8ff7cb32661bf185c221f8
ZhangYiBo1998 <27582180@qq.com>
2025-10-25 02:30:56 +0800

    增加菜单

---
+++ src/pages/home/components/Header.tsx
@@ -0,0 +1,54 @@
+import React, { useState,  memo} from "react";
+import {Flex, type GetProps, Input, Select, Space} from "antd";
+import {useNavigate} from "react-router";
+
+type SearchProps = GetProps<typeof Input.Search>;
+
+const {Search} = Input;
+
+const Header: React.FC = () => {
+    const navigate = useNavigate();
+
+    // 搜索类型
+    const [searchType, setSearchType] = useState('title');
+
+    // 根据类型搜索
+    const onSearch: SearchProps['onSearch'] = async (value) => {
+        if (!value) {
+            return;
+        }
+        const result = await window.electronAPI.findFiles(value, searchType)
+        console.log('搜索结果', result);
+    };
+
+    const options = [
+        {
+            value: 'title',
+            label: '标题',
+        },
+        {
+            value: 'tags',
+            label: '标签',
+        },
+    ];
+
+    return (
+        <Flex className="header">
+            <Flex className="left flex-shrink-0" align="center">
+                <Flex className="logo" justify="center" align="center" onClick={() => navigate("/")}>
+                    <img src="src/assets/icon.png" alt=""/>
+                </Flex>
+                <Flex className="title">Cloud Note</Flex>
+            </Flex>
+            <Flex className="right" justify="center" align="center" style={{padding: "0 20px"}}>
+                <Space.Compact>
+                    <Select style={{width: '100px'}} options={options} value={searchType}
+                            onChange={(value) => setSearchType(value)}/>
+                    <Search style={{width: '400px'}} placeholder={`根据${searchType}搜索`} onSearch={onSearch}/>
+                </Space.Compact>
+            </Flex>
+        </Flex>
+    );
+};
+
+export default memo(Header);
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,61 +1,19 @@
-import React, {memo, useState} from "react";
-import {useNavigate, Outlet} from "react-router";
-import {Flex, Input, Select, Space} from "antd";
-import type {GetProps} from 'antd';
+import React, {memo} from "react";
+import {Flex,} from "antd";
+import Header from "./components/Header";
+import NoteGroups from "./components/NoteGroups";
 import "./index.scss"
 
-const {Search} = Input;
-
-type SearchProps = GetProps<typeof Input.Search>;
-
 const Home: React.FC = () => {
-    const navigate = useNavigate();
-
-    // 搜索类型
-    const [searchType, setSearchType] = useState('title');
-
-    // 根据类型搜索
-    const onSearch: SearchProps['onSearch'] = async (value) => {
-        if (!value) {
-            return;
-        }
-        const result = await window.electronAPI.findFiles(value, searchType)
-        console.log('搜索结果', result);
-    };
-
-    const options = [
-        {
-            value: 'title',
-            label: '标题',
-        },
-        {
-            value: 'tags',
-            label: '标签',
-        },
-    ];
-
     return (
         <Flex id="cloudNoteHome" vertical>
-            <Flex className="header">
-                <Flex className="left flex-shrink-0" justify="center" align="center">
-                    <Flex className="logo" justify="center" align="center" onClick={() => navigate("/")}>
-                        <img src="src/assets/icon.png" alt=""/>
-                    </Flex>
-                    <Flex className="title">Cloud Note</Flex>
-                </Flex>
-                <Flex className="right" justify="center" align="center" style={{padding: "0 20px"}}>
-                    <Space.Compact>
-                        <Select style={{width: '100px'}} options={options} value={searchType}
-                                onChange={(value) => setSearchType(value)}/>
-                        <Search style={{width: '400px'}} placeholder={`根据${searchType}搜索`} onSearch={onSearch}/>
-                    </Space.Compact>
-                </Flex>
-            </Flex>
+            <Header />
             <Flex className="container height-100">
-                <Flex className="sidebar">侧边栏</Flex>
+                <Flex className="sidebar">
+                    <NoteGroups />
+                </Flex>
                 <Flex className="content">
                     编辑区域
-                    <Outlet/>
                 </Flex>
             </Flex>
         </Flex>
